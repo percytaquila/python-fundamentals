2aee477bb529012ce4ae77f199f48e9377a98874
ptaquila <ptaquila@dcp.pe>
2025-11-27 00:54:28 -0500

    feat: Inicio de curso python fundamentals

---
+++ 1. Hola mundo/Ejercicios/HolaMundo.py
@@ -0,0 +1,36 @@
+# Ejercicio 1
+print("Hola Mundo!")
+
+# Ejercicio 2
+# Esto es un comentario de prueba
+
+# Ejercicio 3
+print("Mi nombre es Percy y mi edad es 29 años" )
+
+# Ejercicio 4
+print(type("Texto de prueba"))
+print(type(5))
+print(type(1.5))
+
+# Ejercicio 5
+"""
+Los tipos de datos son los valores que puede guardar en una variable,
+puede ser entero, float, string, complex
+"""
+
+# Ejercicio 6
+print("Hola " + "Mundo")
+
+# Ejercicio 7
+nombre = "Percy"
+edad = 29
+
+print("Mi nombre es " + nombre + " y mi edad es " + str(edad))
+
+
+# Ejercicio 8   
+numero1 = 10
+numero2 = 5
+
+print(numero1 + numero2)
+print(type(numero1 + numero2))
---
+++ 1. Hola mundo/hello.py
@@ -0,0 +1,25 @@
+# Esto es un comentario: Hola mundo
+# Nuestro hola mundo en python
+"""
+Este es un
+comentario 
+en varias lineas
+"""
+
+print("Hola Mundo")
+print('Hola Mundo')
+
+
+'''
+Este tambien es
+un comentario
+en varias lineas
+'''
+
+
+# Consultar el tipo  de dato
+print(type("Hola Python")) # Tipo 'str'
+print(type(5)) # Tipo 'int'
+print(type(1.5)) # Tipo 'float'
+print(type(3 + 1j)) # Tipo 'complex'
+print(type(True)) # Tipo 'bool'
